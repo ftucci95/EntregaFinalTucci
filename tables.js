5f1b06718fcde77a3d7aaef22cea20444e279ffa
ftucci <federicotucci95@gmail.com>
2023-05-29 22:18:02 -0300

    dandole estructura y estilo al html

---
+++ tables.js
@@ -0,0 +1,15 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      // acá tengo que llamar a la función que se ejecuta cuando abro la tabla
+    } 
+  });
+}
